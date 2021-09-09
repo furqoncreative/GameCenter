7753a241e29a67e696519febecafcd8d7867e27a
furqoncreative <furqoncreative24@gmail.com>
2021-09-09 11:48:20 +0700

    Initial Commit

---
+++ GameCenter/GameCenterApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameCenterApp.swift
+//  GameCenter
+//
+//  Created by Dicoding Reviewer on 09/09/21.
+//
+
+import SwiftUI
+
+@main
+struct GameCenterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
